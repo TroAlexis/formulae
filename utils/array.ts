bda592f799495813033b4870b2fe5bbc542b4aad
Alex Troshin <aleksei.troshin@tedo.ru>
2023-02-11 19:56:05 +0800

    feat: add sliceExceptLast array utility

--- utils/array.ts
+++ utils/array.ts
@@ -2,6 +2,10 @@ export const getLast = <T extends any[]>(array: T): T[number] => {
   return array[array.length - 1];
 };
 
+export const sliceExceptLast = <T extends any[]>(array: T) => {
+  return array.slice(0, array.length - 1);
+};
+
 export const spliceLast = <T extends any[], K extends T[number]>(
   array: T,
   el: K
