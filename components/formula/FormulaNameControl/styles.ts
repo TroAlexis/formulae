dbf2d5fb704e5f43a78d2aeec9d62decffdaec98
Alex Troshin <aleksei.troshin@tedo.ru>
2023-02-11 20:02:42 +0800

    fix: fix FormulaNameControl dark mode name color

--- components/formula/FormulaNameControl/styles.ts
+++ components/formula/FormulaNameControl/styles.ts
@@ -1,10 +1,11 @@
 import { createStyles } from "@mantine/core";
+import { isLightTheme } from "config/mantine/utils";
 
 const CONTROL_HEIGHT = 20;
 
 export const useStyles = createStyles((theme) => ({
   nameInput: {
-    color: theme.colors.dark[3],
+    color: isLightTheme(theme) ? theme.colors.dark[3] : theme.colors.dark[0],
     fontWeight: 400,
     height: CONTROL_HEIGHT,
     minHeight: CONTROL_HEIGHT,
---
+++ config/mantine/utils.ts
@@ -0,0 +1,4 @@
+import { MantineTheme } from "@mantine/core";
+
+export const isLightTheme = (theme: MantineTheme) =>
+  theme.colorScheme === "light";
