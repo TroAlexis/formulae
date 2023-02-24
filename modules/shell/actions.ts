a713b3c2f130919fd57742469b1a1503e591f138
Alex Troshin <aleksei.troshin@tedo.ru>
2023-02-24 17:57:39 +0800

    feat(shell): add closeNavbar action

--- modules/shell/actions.ts
+++ modules/shell/actions.ts
@@ -3,6 +3,10 @@ import { ShellActions, ShellStore } from "./models";
 
 const createMutation = createStoreMutationFactory<ShellActions, ShellStore>();
 
-export const toggleNavbar = createMutation("toggleNavbar")((state) => {
-  state.isNavBarOpen = !state.isNavBarOpen;
+export const toggleNavbar = createMutation("toggleNavbar")((state, value) => {
+  state.isNavBarOpen = value ?? !state.isNavBarOpen;
 });
+
+export const closeNavbar = createMutation("closeNavbar")((state) =>
+  toggleNavbar(state, false)
+);
