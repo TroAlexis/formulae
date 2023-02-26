bae27fdc966a4c68428b1bcbe09558bfdf5195b6
Alex Troshin <aleksei.troshin@tedo.ru>
2023-02-26 22:07:02 +0800

    refactor(utils): extract getting last array index into getLastIndex helper

--- utils/array.ts
+++ utils/array.ts
@@ -1,14 +1,21 @@
+export const getLastIndex = (array: any[]): number => {
+  return array.length - 1;
+};
+
 export const getLast = <T extends any[]>(array: T): T[number] => {
-  return array[array.length - 1];
+  const lastIndex = getLastIndex(array);
+  return array[lastIndex];
 };
 
 export const sliceExceptLast = <T extends any[]>(array: T) => {
-  return array.slice(0, array.length - 1);
+  const lastIndex = getLastIndex(array);
+  return array.slice(0, lastIndex);
 };
 
 export const spliceLast = <T extends any[], K extends T[number]>(
   array: T,
   el: K
 ) => {
-  return array.splice(array.length - 1, 1, el);
+  const lastIndex = getLastIndex(array);
+  return array.splice(lastIndex, 1, el);
 };
