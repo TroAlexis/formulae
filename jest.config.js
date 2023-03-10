b098a5734f5bbcfa82542feaf9367b815fb95c2e
Alex Troshin <aleksei.troshin@tedo.ru>
2023-03-10 18:10:47 +0800

    test: add tests for utils/array

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,7 @@ const customJestConfig = {
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 
   testEnvironment: "jest-environment-jsdom",
+  modulePaths: ["<rootDir>"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
