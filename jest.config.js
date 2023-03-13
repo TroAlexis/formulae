12f444c6595852259dee1b31b9d23512dadec600
Alex Troshin <aleksei.troshin@tedo.ru>
2023-03-13 16:33:34 +0800

    test(jest): add setup tests extender

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,7 @@ const customJestConfig = {
 
   testEnvironment: "jest-environment-jsdom",
   modulePaths: ["<rootDir>"],
+  setupFilesAfterEnv: ["<rootDir>/setup-tests.ts"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
